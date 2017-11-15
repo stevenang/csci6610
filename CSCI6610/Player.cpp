39114993c40ece32604b13699376145995b6fb1b
stevenang <stevenang@172.19.76.144>
2017-11-15 07:31:27 -0500

    Initial Commit for Player Class

---
+++ CSCI6610/Player.cpp
@@ -0,0 +1,18 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: 2017年11月15日
+ *      Author: stevenang
+ */
+
+#include "Player.hpp"
+
+Player::Player() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Player::~Player() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CSCI6610/Player.hpp
@@ -0,0 +1,17 @@
+/*
+ * Player.hpp
+ *
+ *  Created on: 2017年11月15日
+ *      Author: stevenang
+ */
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+class Player {
+public:
+	Player();
+	virtual ~Player();
+};
+
+#endif /* PLAYER_HPP_ */
