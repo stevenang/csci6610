783cd094e8ac08e79d14b89fad8d8dd691db12f0
Jayeeta Banerjee <jayeeta1987@outlook.com>
2017-11-27 10:32:31 -0500

    updated version with Q & A round

---
+++ CSCI6610/Decks.hpp
@@ -0,0 +1,43 @@
+/*
+ * Decks.hpp
+ *
+ *  Created on: Nov 18, 2017
+ *      Author: jayee
+ */
+
+#ifndef DECKS_HPP_
+#define DECKS_HPP_
+
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<sstream>
+#include<vector>
+#include <ctime>
+#include <utility>      // std::swap
+#pragma once;
+#include "Deck.hpp"
+using namespace std;
+
+class Decks {
+private:
+	vector<Deck*> deck_list;
+	vector<Card*> all_deck;
+	int white_magic;
+	int totalCard;
+
+public:
+	Decks();
+
+	void fillDecks();
+	void shuffle();
+	vector<Card*> giveCard(int numberOfCards);
+	vector<Card*> getAllDeckCards();
+	void updateAllDeckCards();
+	void printAllDeckCards();
+	void generateWhiteMagicNumber();
+	string getHighCardPercentage();
+	void askQuestion();
+};
+
+#endif /* DECKS_HPP_ */
